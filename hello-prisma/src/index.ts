3548dfae2b53050da457c28ebc6bbaa336464976
Mostafiz <mostafiz4372@gmail.com>
2025-09-11 21:57:28 +0600

    learning prisma

---
+++ hello-prisma/src/index.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient()
+async function main(){
+    // create data
+    // const result = await prisma.user.create({
+    //     data: {
+    //         name: "Jhankar Mahbub",
+    //         email: "jkh@gmail.com",
+    //         profilePhoto: "https://programming-hero-ceo-jkr.png"
+    //     }
+    // })
+
+    // get data with prisma
+    const result = await prisma.user.findMany();
+    console.log(result);
+    
+    
+}
+
+main();
