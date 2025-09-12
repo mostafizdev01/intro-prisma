690787d20d761f01684a907660e9b6cb7a1a585e
Mostafiz <mostafiz4372@gmail.com>
2025-09-12 12:46:17 +0600

    all practices query in prisma

--- hello-prisma/dist/index.js
+++ hello-prisma/dist/index.js
@@ -21,9 +21,67 @@ async function main() {
     // const result = await prisma.user.findMany();
     // console.log(result);
     // // find uniq data with prisma
-    const result = await prisma.user.findUnique({
-        where: {
-            id: 4
+    // const result = await prisma.user.findUnique({
+    //     where: {
+    //         id: 4
+    //     }
+    // });
+    // console.log(result);
+    // // find unique or error data with prisma
+    // const result = await prisma.user.findUniqueOrThrow({
+    //     where: {
+    //         id: 9
+    //     }
+    // });
+    // console.log(result);
+    // // // Update data with prisma
+    // const result = await prisma.user.update({
+    //     where: {
+    //         id: 2
+    //     },
+    //     data: {
+    //         name: "Rasel"
+    //     }
+    // });
+    // console.log(result);
+    // // // Update many data with prisma
+    // const result = await prisma.user.update({
+    //     where: {
+    //         id: 2
+    //     },
+    //     data: {
+    //         profilePhoto: "https://www.programming-hero.png"
+    //     }
+    // });
+    // console.log(result);
+    // // // UpdateManyAndReturn data with prisma
+    // const result = await prisma.user.updateManyAndReturn({
+    //     where: {
+    //         id: {gt: 2}
+    //     },
+    //     data: {
+    //         profilePhoto: "https://www.programming-hero.png"
+    //     }
+    // });
+    // console.log(result);
+    // // delete data with prisma
+    // const result = await prisma.user.delete({
+    //     where: {
+    //         id: 2
+    //     }
+    // });
+    // console.log(result);
+    // // // delete data many with prisma
+    // const result = await prisma.user.deleteMany({
+    //     where: {
+    //         id: {lt: 3}
+    //     }
+    // });
+    // console.log(result);
+    // // sorting data many with prisma
+    const result = await prisma.user.findMany({
+        orderBy: {
+            id: "desc"
         }
     });
     console.log(result);
