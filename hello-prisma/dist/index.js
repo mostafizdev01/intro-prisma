794be28637a351e6f177c9379b1259ad1c4d9faf
Mostafiz <mostafiz4372@gmail.com>
2025-09-12 11:26:04 +0600

    find uniq data with prisma

--- hello-prisma/dist/index.js
+++ hello-prisma/dist/index.js
@@ -9,8 +9,23 @@ async function main() {
     //         profilePhoto: "https://programming-hero-ceo-jkr.png"
     //     }
     // })
-    // get data with prisma
-    const result = await prisma.user.findMany();
+    // const users = await prisma.user.createMany({
+    //     data: [
+    //         { name: "Mir", email: "mir@ph.com" },
+    //         { name: "Tanmoy", email: "tanmoy@ph.com" },
+    //         { name: "Mizan", email: "mizan@ph.com" },
+    //         { name: "Imun", email: "imun@ph.com" }
+    //     ]
+    // });
+    // // get data with prisma
+    // const result = await prisma.user.findMany();
+    // console.log(result);
+    // // find uniq data with prisma
+    const result = await prisma.user.findUnique({
+        where: {
+            id: 4
+        }
+    });
     console.log(result);
 }
 main();
--- hello-prisma/src/index.ts
+++ hello-prisma/src/index.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 
 const prisma = new PrismaClient()
-async function main(){
+async function main() {
     // create data
     // const result = await prisma.user.create({
     //     data: {
@@ -12,11 +12,27 @@ async function main(){
     //     }
     // })
 
-    // get data with prisma
-    const result = await prisma.user.findMany();
+    // const users = await prisma.user.createMany({
+    //     data: [
+    //         { name: "Mir", email: "mir@ph.com" },
+    //         { name: "Tanmoy", email: "tanmoy@ph.com" },
+    //         { name: "Mizan", email: "mizan@ph.com" },
+    //         { name: "Imun", email: "imun@ph.com" }
+    //     ]
+    // });
+
+    // // get data with prisma
+    // const result = await prisma.user.findMany();
+    // console.log(result);
+
+    // // find uniq data with prisma
+    const result = await prisma.user.findUnique({
+        where: {
+            id: 4
+        }
+    });
     console.log(result);
-    
-    
+
 }
 
 main();
